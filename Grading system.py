dce164841fd881e304fd9ceb0aaad495c7c465da
DEV-PATEL <DEVP59237@GMAIL.com>
2025-10-02 01:55:24 +0530

    grading system

---
+++ Grading system.py
@@ -0,0 +1,17 @@
+Marks =float(input("enter your marks :77"))
+if Marks>=90 and Marks<=100:
+    print("Grade A")
+elif Marks>=80 and Marks<=90:
+    print("the grade is B")
+elif(Marks>=70 and Marks<=80):
+    print("the grade is C")
+elif(Marks>=60 and Marks<=70):
+    print("the grade is D")
+elif(Marks>=50 and Marks<=60):
+    print("the grade is E")
+elif(Marks<50 and Marks>=0):
+    print("the grade is F")
+else:
+    print("the input is invalid")
+
+

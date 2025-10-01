97a1f743cfd4fbbcc7449662214f81d4acc885fb
DEV-PATEL <DEVP59237@GMAIL.com>
2025-10-02 00:40:42 +0530

    input and float

---
+++ userinfo.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gpa = float(input("Enter your GPA: "))
+print("\n --- User Information ---")
+print(f"Name: {name}")
+print(f"Age: {age} years old")
+print(f"GPA: {gpa}")
+
